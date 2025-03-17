2124f3235a25a169d3a77f77436d80959e65cea4
kleber <lab53@fatec.com>
2025-03-17 10:05:20 -0300

    Contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("aperte enter para contar");
+Console.ReadKey();
+
+Console.WriteLine("preparar...");
+Console.ReadKey();
+
+Console.WriteLine("apontar...");
+Console.ReadKey();
+
+Console.WriteLine("FOGO!");
+Console.ReadKey();
+
